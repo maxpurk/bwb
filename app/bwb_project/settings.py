0be5742be2290dc361df062016c6ae185f00d070
maxpurk <maximilian.purk@icloud.com>
2023-12-21 21:48:28 +0100

    added some functions

--- app/bwb_project/settings.py
+++ app/bwb_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles", 
+    "upload",
 ]
 
 MIDDLEWARE = [
@@ -124,8 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
